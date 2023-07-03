91f33ca83c373fb5c609d5ca1ecdffffec622a66
Jonathan Garcia <133997935+JGV-07@users.noreply.github.com>
2023-07-03 00:47:36 -0500

    Adelantando lo que mas pude, falta bastante CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,59 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
 
 function App() {
 
+  const [ weatherData, setWeatherData ] = useState({})
+  const [ isFahrenheit, setIsFahrenheit ] = useState(true)
+  const [ isDarkMode, setIsDarkMode ] = useState(false)
+
+  const changeFc = () => {
+     setIsFahrenheit(!isFahrenheit)
+  }
+
+  const changeToDarkMode = () => {
+    setIsDarkMode(!isDarkMode)
+    document.body.classList.toggle('dark-mode')
+  }
+
+useEffect (() =>{
+  navigator.geolocation.getCurrentPosition((position) => {
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
+
+    
+    
+    axios
+    .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=4929ea72ba6ebbcdc42ee03f8228b265`)
+    .then((resp) => {
+      setWeatherData(resp.data)
+    })
+    .catch((error) => console.error(error))
+  });
+  
+}, []) 
+
+const toKelvin = weatherData.main?.temp;
+const toCelcius = Math.round(toKelvin - 273.15)
+const toFahrenheit = Math.round((toCelcius * 9/5) + 32)
+
   return (
-    <>
+    <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
       <div className="header">
-        <h1 className='title'>Weather app</h1>
-        <button className='buttonDarkMode'>Dark Mode</button>
+        <h2 className='title'>Weather app</h2>
+        <button onClick={changeToDarkMode}  className='buttonDarkMode'>Dark Mode</button>
       </div>
       <div className='container'>
-        Temperatura en grados centigrados,
-        Pais, 
-        Ciudad,
-        Icono que describa el clima,
-        Icono que corresponda al clima.
+        <h1>{isFahrenheit ? toFahrenheit : toCelcius} {isFahrenheit ? "°F" : "°C"}</h1>
+        <p>{weatherData.weather?.[0].main}</p>
+        <p>Pressure: {weatherData.main?.pressure}</p>
+        <p>{weatherData.weather?.[0].description}</p>
+        <h2>{weatherData.sys?.country}</h2>
+        <h2>{weatherData.name}</h2>
       </div>
-      <button className='buttonCh'>Boton para cambiar de F a C</button>
-    </>
+      <button onClick={changeFc}  className='buttonCh'>{isFahrenheit ? "Change to °C" : "Change to °F"}</button>
+    </div>
   )
 }
 
