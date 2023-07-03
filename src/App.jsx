b819ccef91e45f18a4a2859844d9bd3f92a484ef
Jonathan Garcia <133997935+JGV-07@users.noreply.github.com>
2023-07-03 12:49:48 -0500

    agregando la funcionalidad y algunos aspectos del CSS

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,20 @@ function App() {
   const [ isFahrenheit, setIsFahrenheit ] = useState(true)
   const [ isDarkMode, setIsDarkMode ] = useState(false)
 
+  const icons = {
+
+    "01d": "/icons/01d.svg",
+    "02d": "/icons/02d.svg",
+    "03d": "/icons/03d.svg",
+    "04d": "/icons/04d.svg",
+    "05d": "/icons/05d.svg",
+    "06d": "/icons/06d.svg",
+    "07d": "/icons/07d.svg",
+    "08d": "/icons/08d.svg",
+    "09d": "/icons/09d.svg"
+
+  }
+
   const changeFc = () => {
      setIsFahrenheit(!isFahrenheit)
   }
@@ -21,9 +35,7 @@ useEffect (() =>{
   navigator.geolocation.getCurrentPosition((position) => {
     const latitude = position.coords.latitude
     const longitude = position.coords.longitude
-
-    
-    
+        
     axios
     .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=4929ea72ba6ebbcdc42ee03f8228b265`)
     .then((resp) => {
@@ -42,15 +54,17 @@ const toFahrenheit = Math.round((toCelcius * 9/5) + 32)
     <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
       <div className="header">
         <h2 className='title'>Weather app</h2>
-        <button onClick={changeToDarkMode}  className='buttonDarkMode'>Dark Mode</button>
+        <button onClick={changeToDarkMode}  className='buttonDarkMode'><i className='bx bx-toggle-left bx-lg' ></i></button>
       </div>
       <div className='container'>
         <h1>{isFahrenheit ? toFahrenheit : toCelcius} {isFahrenheit ? "°F" : "°C"}</h1>
+        <img className="image" src={icons[weatherData.weather?.[0].icon]} alt="image of weather" />
         <p>{weatherData.weather?.[0].main}</p>
         <p>Pressure: {weatherData.main?.pressure}</p>
-        <p>{weatherData.weather?.[0].description}</p>
-        <h2>{weatherData.sys?.country}</h2>
-        <h2>{weatherData.name}</h2>
+        <div className='container-country'>
+          <h2>{weatherData.name},{weatherData.sys?.country}</h2>
+          <p>{weatherData.weather?.[0].description}</p>
+        </div>
       </div>
       <button onClick={changeFc}  className='buttonCh'>{isFahrenheit ? "Change to °C" : "Change to °F"}</button>
     </div>
@@ -58,3 +72,9 @@ const toFahrenheit = Math.round((toCelcius * 9/5) + 32)
 }
 
 export default App
+
+
+/*  ICONOS PARA EL BOTON DE DARK MODE
+<i class='bx bx-toggle-left' ></i>
+<i class='bx bx-toggle-right' ></i>
+*/
